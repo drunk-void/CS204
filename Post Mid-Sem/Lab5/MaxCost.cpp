74f812835a29262adf02901b023d34a0924a0e80
“mchandolia” <mchandolia35@gmail.com>
2019-11-04 16:28:56 +0530

    .

--- Post Mid-Sem/Lab5/MaxCost.cpp
+++ Post Mid-Sem/Lab5/MaxCost.cpp
@@ -5,5 +5,14 @@ using namespace std;
 class Graph {
     ll V;
     vector<vector<ll>> adj;
+    void DFSUtil(int, bool visted[]);
+    public :
+        Graph(int V);
+        void addEdge(int v, int w);
+        void DFS(int v); 
+};
+
+Graph::Graph(int V) {
+    this->V=V;
     
 }
